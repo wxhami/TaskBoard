8610d70723ff8db2615638ee2077f0af973048f5
Диана Агапова <d18102002@mail.ru>
2025-04-11 20:22:16 +0300

    added MyTask entity

---
+++ Domain/Entities/MyTask.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class MyTask
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public MyTaskStatus Status { get; set; }
+    public TaskPriority Priority { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? DueDate { get; set; }
+    public string? UserId { get; set; }
+}
